99a7400a42b9d564c726154d0c851700f7758e38
Owen Singleton <owen.singleton@gmail.com>
2012-04-21 09:16:11 +0100

    now provides XML output with simple XML tests

--- lib/Menu/Scraper.pm
+++ lib/Menu/Scraper.pm
@@ -1,9 +1,12 @@
 package Menu::Scraper;
 
-use 5.006;
 use strict;
 use warnings;
+
 use File::Basename;
+use XML::Writer;
+use XML::Writer::String;
+
 my @file;
 my $file;
 my $menu;
@@ -80,6 +83,76 @@ sub get_menu {
 }
 
 =head2 get_menu
+
+=cut
+
+sub get_menu_xml {
+  my $self = shift;
+
+  my $menu = get_menu();
+  
+  my $xml = new XML::Writer::String;
+  my $writer = XML::Writer->new(OUTPUT => $xml, NEWLINES=>0, DATA_MODE=>1);
+  
+  $writer->startTag('omf','uuid'=>'1234','created_date'=>'9-APRIL-2012','accuracy'=>'poor');
+  
+  $writer->startTag('restaurant_info');
+  
+  $writer->startTag('restaurant_name');
+  $writer->characters($menu->{name});
+  $writer->endTag('restaurant_name');
+  
+  $writer->startTag('address_1');
+  $writer->characters($menu->{address});
+  $writer->endTag('address_1');
+  
+  $writer->startTag('environment');
+  $writer->startTag('operating_days');
+  
+  my $hours = $menu->{hours};
+  foreach my $key (sort keys %$hours) {
+  
+    $writer->startTag('operating_day');
+  
+    $writer->startTag('day_of_week');
+    $writer->characters($key);
+    $writer->endTag('day_of_week');
+    
+    $writer->startTag('open_time');
+    $writer->characters($hours->{$key}->{open});
+    $writer->endTag('open_time');
+    
+    $writer->startTag('close_time');
+    $writer->characters($hours->{$key}->{close});
+    $writer->endTag('close_time');
+  
+    $writer->endTag('operating_day');
+  
+  }
+  
+  $writer->endTag('operating_days');
+  
+  $writer->endTag('environment');
+  
+  my $items = $menu->{items};
+  foreach my $key (sort keys %$items) {
+    $writer->startTag('menu_item','uid'=>$key);
+    $writer->dataElement('menu_item_name', $items->{$key}->{name} );
+    $writer->dataElement('menu_item_size', $items->{$key}->{size} );
+    $writer->dataElement('menu_item_price', $items->{$key}->{price} );
+    $writer->endTag('menu_item');
+  }
+  
+  $writer->endTag('restaurant_info');
+  
+  $writer->endTag('omf');
+  $writer->end();
+
+  return $xml->value();
+
+}
+
+=head2 get_menu_xml
 
 =cut
 
